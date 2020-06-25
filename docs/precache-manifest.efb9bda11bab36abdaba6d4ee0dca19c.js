fd70eb8b5407573a2ad371c2d875052b5d7f6dca
Rohit Anand <rohitanand3456@gmail.com>
2020-06-25 17:23:20 +0530

    added build to docs folder

---
+++ docs/precache-manifest.efb9bda11bab36abdaba6d4ee0dca19c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2bd8de31a253a504fd50ff8c0c30013",
+    "url": "/ingredient-builder/index.html"
+  },
+  {
+    "revision": "69291fe6dde2654f238b",
+    "url": "/ingredient-builder/static/css/main.a9763042.chunk.css"
+  },
+  {
+    "revision": "f6847ecdcc07b30797fe",
+    "url": "/ingredient-builder/static/js/2.f97e73df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ingredient-builder/static/js/2.f97e73df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69291fe6dde2654f238b",
+    "url": "/ingredient-builder/static/js/main.6389652d.chunk.js"
+  },
+  {
+    "revision": "ff17ee6333a3df566db4",
+    "url": "/ingredient-builder/static/js/runtime-main.864ada40.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/ingredient-builder/static/media/logo.b8503d26.png"
+  }
+]);
